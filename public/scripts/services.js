777c8d0fd90f743dcd8f972aa522ff658a8141c2
Dhruv Mongia <dhruv.mongia@gmail.com>
2017-05-03 01:08:04 +0530

    fixed bugs

--- public/scripts/services.js
+++ public/scripts/services.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('confusionApp')
-.constant("baseURL", "https://localhost:3443/")
+.constant("baseURL", "http://localhost:3000/")
 .factory('menuFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
 
         return $resource(baseURL + "dishes/:id", null, {
