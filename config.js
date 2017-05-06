90051a55c351caf06bcd29b0afe979aa8c52e939
Dhruv Mongia <dhruv.mongia@gmail.com>
2017-05-07 01:03:15 +0530

    hided secret keys

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://dhruv:abc@ds137730.mlab.com:37730/confusion',
+    'secretKey': process.env.secretKey,
+    'mongoUrl' : process.env.mongoUrl,
     'facebook': {
-        clientID: '432948410371020',
-        clientSecret: 'd7b8565556425f9a6332271abc4a80a4',
+        clientID: process.env.clientID,
+        clientSecret: process.env.clientSecret,
         callbackURL: 'https://localhost:3443/users/facebook/callback'
     }
 }
